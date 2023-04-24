78a125591375ea5d779d966d9684cdf9bf701b5d
ayeshaishaikh <87683724+ayeshaishaikh@users.noreply.github.com>
2023-04-24 06:07:28 +0000

    A

---
+++ Day_3_1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Day_3_1 
+{
+    public static void main(String args[])
+    /*{
+        int a=2;
+        switch(a)
+        {
+            case 1:
+            System.out.println("1");
+            break;
+
+            case 2:
+            System.out.println("2");
+            break;
+
+            case 3:
+            System.out.println("3");
+            break;
+            
+        }
+
+    }*/
+
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1 ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter num2 ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Select operation ");
+        char op = sc.next().charAt(0);
+
+        System.out.println("addition - a , subtraction -s , multiplication - m, dividion-d");
+
+        
+    }
+    
+}
