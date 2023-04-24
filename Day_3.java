2959df98414a9042d15b006f93a680b7b85da908
ayeshaishaikh <87683724+ayeshaishaikh@users.noreply.github.com>
2023-04-24 04:57:32 +0000

    fDay_3 of cdp

---
+++ Day_3.java
@@ -0,0 +1,92 @@
+import javax.lang.model.util.ElementScanner14;
+
+class Day_3
+{
+    public static void main (String args[])
+    /*{
+        int a = 10;
+        int b=20;
+        int c=30;
+        if(a<b)
+        {
+            System.out.println("b is greater than a");
+        }
+        else
+        {
+            System.out.println("a is greater than b");
+        }
+
+    }*/
+
+    /* {
+        int a = 10;
+        int b=20;
+        int c=30;
+        if(a<b && a<c)
+        {
+            System.out.println("a is smallest number");
+        }
+        else if(a>b && a>c)
+        {
+            System.out.println("a is largest number");
+        }
+        else
+        {
+            System.out.println("we cant decide ");
+        }
+        
+    }*/
+
+    /*{
+        int a=20;
+        int b=90;
+        int c=60;
+        
+        if(a<b && a<c)
+        {
+             a = b+c;
+            System.out.println("new value of a is :" + a);
+        }
+        else
+        {
+            System.out.println("original value of a is : " + a);
+
+        }
+    }*/
+
+    /*{
+        int maths=60;
+        int science = 78;
+        int history = 23;
+        int hindi = 80;
+        int geogrophy = 56;
+
+        int sum= maths + science + history + hindi + geogrophy ;
+        
+        System.out.println("The sum is : " + sum );
+
+        int avg= sum/5;
+        System.out.println("The avg  % is : " + avg +"%");
+
+        if(avg >75)
+        {
+            System.out.println("congragts u scored well and get distiction");
+        }
+        else if(avg>60 && avg<75)
+        {
+            System.out.println("congrats u get first class");
+        }
+
+        else (avg>35 && avg<60)
+        {
+            System.out.println("u need to study ");
+        }
+        
+
+
+    }*/
+
+    
+
+
+}
